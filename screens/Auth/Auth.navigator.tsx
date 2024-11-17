7f9bdd70df191ac9a2b3e8efe7482cd928afc968
Daniel Andersson <d.andersson86@icloud.com>
2024-11-17 17:04:28 +0100

    replace React.JSX.Element with React.ReactElement in component Auth.navigator.tsx and add descriptive docstrings

--- screens/Auth/Auth.navigator.tsx
+++ screens/Auth/Auth.navigator.tsx
@@ -1,49 +1,22 @@
 /**
+ * Auth Navigator module.
+ *
+ * This module defines the authentication navigator for the application.
+ * It includes screens for user login and registration.
+ *
  * @module AuthNavigator
- *
- * This module sets up a stack navigator for the authentication-related screens in the application.
- * Depending on the user's authentication status, it either shows the invoice navigator or the
- * login/register screens.
- * It includes:
- * - Login: Screen for user login.
- * - Register: Screen for user registration.
- * - InvoiceNavigator: Navigator for invoice-related screens, shown if the user is logged in.
- *
- * The navigator is wrapped in a SafeAreaView to ensure it is displayed correctly on all devices.
- * It also includes a FlashMessage component for displaying notifications.
- *
- * @requires react
- * @requires react-native
- * @requires react-native-screens
- * @requires react-native-flash-message
- * @requires ../../context/Auth.provider
- * @requires ./Login.screen
- * @requires ./Register.screen
- * @requires ../Invoices/Invoices.navigator
- * @requires ../../assets/styles
  */
-// External libraries
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NativeStackNavigatorProps } from 'react-native-screens/lib/typescript/native-stack/types';
-import FlashMessage from 'react-native-flash-message';
 
-// Internal components and modules
-import { useAuthContext } from '../../context/Auth.provider';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import FlashMessage from 'react-native-flash-message';
 import { Login } from './Login.screen';
 import { Register } from './Register.screen';
-import { InvoiceNavigator } from '../Invoices/Invoices.navigator';
-
-// Assets & styles
 import * as Style from '../../assets/styles';
+import { StatusBar } from 'expo-status-bar';
 
-
-/**
- * Stack navigator for authentication.
- */
-const Stack: NativeStackNavigatorProps = createStackNavigator();
-
+const Stack = createStackNavigator();
 
 /**
  * AuthNavigator component.
@@ -56,20 +29,11 @@ const Stack: NativeStackNavigatorProps = createStackNavigator();
  * @returns {React.ReactElement} The authentication stack navigator component.
  */
 export const AuthNavigator: React.FC = (): React.ReactElement => {
-    const authContext = useAuthContext();
-
+    // const authContext = useAuthContext();
     return (
         <SafeAreaView style={Style.Base.mainContainer}>
-            <Stack.Navigator initianRouteName='Logga in formulär'>
-                {authContext.isLoggedIn ? (
-                    <Stack.Screen
-                        name='Faktura'
-                        component={InvoiceNavigator}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
-                ) : (
+            <Stack.Navigator initialRouteName='Logga in formulär'>
+                {
                     <>
                         <Stack.Screen
                             name='Logga in formulär'
@@ -86,10 +50,12 @@ export const AuthNavigator: React.FC = (): React.ReactElement => {
                             }}
                         />
                     </>
-                )}
+                }
             </Stack.Navigator>
 
-            <FlashMessage position="top"/>
+            <StatusBar style='auto' />
+
+            <FlashMessage position='top' />
         </SafeAreaView>
     );
 };
