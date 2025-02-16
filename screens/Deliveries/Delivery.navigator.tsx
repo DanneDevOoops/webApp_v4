f571b43165657d7ad9b50347f57beb48fb2d6738
Daniel Andersson <d.andersson86@icloud.com>
2025-02-16 18:18:38 +0100

    update module Delivery.navigator.tsx

--- screens/Deliveries/Delivery.navigator.tsx
+++ screens/Deliveries/Delivery.navigator.tsx
@@ -9,43 +9,28 @@
  *
  * The navigator is wrapped in a SafeAreaView to ensure it is displayed correctly on all devices.
  * It also includes a FlashMessage component for displaying notifications and a StatusBar component.
- *
- * @requires react
- * @requires react-native
- * @requires react-native-screens
- * @requires react-native-flash-message
- * @requires expo-status-bar
- * @requires ../../components/Delivery/DeliveryList
- * @requires ./DeliveryItem.screen
- * @requires ./DeliveryForm.screen
- * @requires ../../components/Utils/CoverImage
- * @requires ../../assets/styles
- * @requires ../../assets/img/NutsAndBolts-6.jpg
  */
 // External libraries
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-import {NativeStackNavigatorProps} from 'react-native-screens/lib/typescript/native-stack/types';
-import {createStackNavigator} from '@react-navigation/stack';
-import {StatusBar} from 'expo-status-bar';
+import { SafeAreaView, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
 import FlashMessage from 'react-native-flash-message';
 
 // Internal components and modules
-import {DeliveryList} from '../../components/Delivery/DeliveryList';
-import {DeliveryItem} from './DeliveryItem.screen';
-import {DeliveryCreationForm} from './DeliveryForm.screen';
-import {CoverImage} from '../../components/Utils/CoverImage';
+import { DeliveryList } from '../../components/Delivery/DeliveryList';
+import { DeliveryItem } from './DeliveryItem.screen';
+import { DeliveryCreationForm } from './DeliveryForm.screen';
+import { CoverImage } from '../../components/Utils/CoverImage';
 
 // Assets & styles
 import coverIMG from '../../assets/img/NutsAndBolts-6.jpg';
 import * as Style from '../../assets/styles';
 
-
 /**
  * Stack navigator for deliveries.
  */
-const Stack: NativeStackNavigatorProps = createStackNavigator();
-
+const Stack = createStackNavigator();
 
 /**
  * Delivery Stack Navigator.
@@ -82,13 +67,14 @@ export const DeliveryNavigator: React.FC = (): React.ReactElement => {
                 />
                 <Stack.Screen
                     name='Inleverasformulär'
+                    r
                     component={DeliveryCreationForm}
                 />
             </Stack.Navigator>
 
             <StatusBar style='auto' />
 
-            <FlashMessage position="top"/>
+            <FlashMessage position='top' />
         </SafeAreaView>
     );
 };
