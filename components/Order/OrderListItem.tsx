03c0c10c8b55b5685b0647d985f5b232c51f5908
Daniel Andersson <d.andersson86@icloud.com>
2025-02-16 18:28:40 +0100

    Update OrderListItem.tsx return type

--- components/Order/OrderListItem.tsx
+++ components/Order/OrderListItem.tsx
@@ -2,7 +2,7 @@
  * Module imports.
  */
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextStyle, View, ViewStyle } from 'react-native';
 import * as Style from '../../assets/styles';
 
 /**
@@ -22,14 +22,18 @@ type OrderListItemPropsType = {
  * @constructor
  * @param props
  */
-export const OrderListItem = (props): React.JSX.Element => {
+export const OrderListItem = (props): React.ReactElement => {
     return (
         <View
             key={props.item.id}
-            style={Style.Container.grid.row}>
-            <Text style={Style.Typography.dataLeft}>{props.item.id}</Text>
-            <Text style={Style.Typography.dataCenter}>{props.item.name}</Text>
-            <Text style={Style.Typography.dataRight}>
+            style={Style.Container.grid.row as ViewStyle}>
+            <Text style={Style.Typography.dataLeft as TextStyle}>
+                {props.item.id}
+            </Text>
+            <Text style={Style.Typography.dataCenter as TextStyle}>
+                {props.item.name}
+            </Text>
+            <Text style={Style.Typography.dataRight as TextStyle}>
                 {props.item.status_id}
             </Text>
         </View>
