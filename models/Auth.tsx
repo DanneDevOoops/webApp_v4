58ef44542602a5bd33940e63b8d4d9a1c4c646d2
Daniel Andersson <d.andersson86@icloud.com>
2025-02-16 18:17:32 +0100

    import app config from config.json in Auth.tsx

--- models/Auth.tsx
+++ models/Auth.tsx
@@ -1,4 +1,4 @@
-import config from "../config/config.json";
+import config from '../config/config.json';
 import * as SecureStore from 'expo-secure-store';
 import * as AuthInterfaces from '../interfaces/Auth.interfaces';
 
