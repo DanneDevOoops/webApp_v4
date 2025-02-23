377a46fb619403e2ee5b828014ae6cac39e6218d
Daniel Andersson <d.andersson86@icloud.com>
2025-02-23 22:00:53 +0100

    add interfaces NumKeyStrValuesAttributeType, StrKeyNumValueAttributeType, NumKeyNumValueAttributeType, StrKeyStrValueAttributeType and rename ColorAttributeType
    Took 31 minutes

--- assets/styles/colors.ts
+++ assets/styles/colors.ts
@@ -10,9 +10,9 @@
 
 import {
     BgColorInterface,
-    ColorAttributeType,
     ColorResponseInterface,
     ColorSchemeInterface,
+    NumKeyStrValuesAttributeType,
     TextColorInterface,
 } from '../../types/StyleTypes';
 
@@ -131,7 +131,7 @@ export const text: TextColorInterface = {
  * @property {string} 900 - The darkest gray color.
  * @property {string} 1000 - The deepest gray color.
  */
-export const grayScale: ColorAttributeType = {
+export const grayScale: NumKeyStrValuesAttributeType = {
     50: '#F2F2F2',
     100: '#BFBFBF',
     200: '#8C8C8C',
@@ -306,7 +306,7 @@ export const indicator: ColorResponseInterface = {
  * @property {string} 400 - A dark shadow color.
  * @property {string} 500 - The darkest shadow color.
  */
-export const shadows: ColorAttributeType = {
+export const shadows: NumKeyStrValuesAttributeType = {
     100: '#BFBFBF',
     200: '#8C8C8C',
     300: '#3F3F3F',
--- types/StyleTypes.ts
+++ types/StyleTypes.ts
@@ -1,70 +1,112 @@
 /**
- * Represents a color attribute type where the key is a number and the value is a string.
+ * @module StyleTypes
+ *
+ * This module defines various TypeScript interfaces for styling attributes.
+ *
+ * It includes interfaces for objects with numeric and string keys and values, as well as interfaces
+ * for color schemes, color responses, background colors, and text colors.
  */
-export interface ColorAttributeType {
+
+/**
+ * Interface representing an object with numeric keys and string values.
+ *
+ * @interface NumKeyStrValuesAttributeType
+ * @property {string} [key: number] - The value associated with the numeric key.
+ */
+export interface NumKeyStrValuesAttributeType {
     [key: number]: string;
+}
+
+/**
+ * Interface representing an object with string keys and numeric values.
+ *
+ * @interface StrKeyNumValueAttributeType
+ * @property {number} [key: string] - The value associated with the string key.
+ */
+export interface StrKeyNumValueAttributeType {
+    [key: string]: number;
+}
+
+/**
+ * Interface representing an object with numeric keys and numeric values.
+ *
+ * @interface NumKeyNumValueAttributeType
+ * @property {number} [key: number] - The value associated with the numeric key.
+ */
+export interface NumKeyNumValueAttributeType {
+    [key: number]: number;
+}
+
+/**
+ * Interface representing an object with string keys and string values.
+ *
+ * @interface StrKeyStrValueAttributeType
+ * @property {string} [key: string] - The value associated with the string key.
+ */
+export interface StrKeyStrValueAttributeType {
+    [key: string]: string;
 }
 
 /**
  * Represents a color scheme interface with primary, secondary, tertiary, quarternary, and quintary color attributes.
  *
- * @property {ColorAttributeType} primary - Primary color attributes.
- * @property {ColorAttributeType} secondary - Secondary color attributes.
- * @property {ColorAttributeType} tertiary - Tertiary color attributes.
- * @property {ColorAttributeType} quarternary - Quarternary color attributes.
- * @property {ColorAttributeType} quintary - Quintary color attributes.
+ * @property {NumKeyStrValuesAttributeType} primary - Primary color attributes.
+ * @property {NumKeyStrValuesAttributeType} secondary - Secondary color attributes.
+ * @property {NumKeyStrValuesAttributeType} tertiary - Tertiary color attributes.
+ * @property {NumKeyStrValuesAttributeType} quarternary - Quarternary color attributes.
+ * @property {NumKeyStrValuesAttributeType} quintary - Quintary color attributes.
  */
 export interface ColorSchemeInterface {
-    primary: ColorAttributeType;
-    secondary: ColorAttributeType;
-    tertiary: ColorAttributeType;
-    quarternary: ColorAttributeType;
-    quintary: ColorAttributeType;
+    primary: NumKeyStrValuesAttributeType;
+    secondary: NumKeyStrValuesAttributeType;
+    tertiary: NumKeyStrValuesAttributeType;
+    quarternary: NumKeyStrValuesAttributeType;
+    quintary: NumKeyStrValuesAttributeType;
 }
 
 /**
  * Represents a color response interface with various indicator colors.
  *
- * @property {ColorAttributeType} info - Info indicator colors.
- * @property {ColorAttributeType} positive - Positive indicator colors.
- * @property {ColorAttributeType} [success] - Success indicator colors (optional).
- * @property {ColorAttributeType} caution - Caution indicator colors.
- * @property {ColorAttributeType} warning - Warning indicator colors.
- * @property {ColorAttributeType} [critical] - Critical indicator colors (optional).
- * @property {ColorAttributeType} link - Link indicator colors.
- * @property {ColorAttributeType} visited - Visited link indicator colors.
+ * @property {NumKeyStrValuesAttributeType} info - Info indicator colors.
+ * @property {NumKeyStrValuesAttributeType} positive - Positive indicator colors.
+ * @property {NumKeyStrValuesAttributeType} [success] - Success indicator colors (optional).
+ * @property {NumKeyStrValuesAttributeType} caution - Caution indicator colors.
+ * @property {NumKeyStrValuesAttributeType} warning - Warning indicator colors.
+ * @property {NumKeyStrValuesAttributeType} [critical] - Critical indicator colors (optional).
+ * @property {NumKeyStrValuesAttributeType} link - Link indicator colors.
+ * @property {NumKeyStrValuesAttributeType} visited - Visited link indicator colors.
  */
 export interface ColorResponseInterface {
-    info: ColorAttributeType;
-    positive: ColorAttributeType;
-    success?: ColorAttributeType;
-    caution: ColorAttributeType;
-    warning: ColorAttributeType;
-    critical?: ColorAttributeType;
-    link: ColorAttributeType;
-    visited: ColorAttributeType;
+    info: NumKeyStrValuesAttributeType;
+    positive: NumKeyStrValuesAttributeType;
+    success?: NumKeyStrValuesAttributeType;
+    caution: NumKeyStrValuesAttributeType;
+    warning: NumKeyStrValuesAttributeType;
+    critical?: NumKeyStrValuesAttributeType;
+    link: NumKeyStrValuesAttributeType;
+    visited: NumKeyStrValuesAttributeType;
 }
 
 /**
  * Represents background color interface with light and dark color attributes.
  *
- * @property {ColorAttributeType} light - Light background color attributes.
- * @property {ColorAttributeType} dark - Dark background color attributes.
+ * @property {NumKeyStrValuesAttributeType} light - Light background color attributes.
+ * @property {NumKeyStrValuesAttributeType} dark - Dark background color attributes.
  */
 export interface BgColorInterface {
-    light: ColorAttributeType;
-    dark: ColorAttributeType;
+    light: NumKeyStrValuesAttributeType;
+    dark: NumKeyStrValuesAttributeType;
 }
 
 /**
  * Represents text color interface with light, dark, and disabled color attributes.
  *
- * @property {ColorAttributeType} light - Light text color attributes.
- * @property {ColorAttributeType} dark - Dark text color attributes.
- * @property {ColorAttributeType} disabled - Disabled text color attributes.
+ * @property {NumKeyStrValuesAttributeType} light - Light text color attributes.
+ * @property {NumKeyStrValuesAttributeType} dark - Dark text color attributes.
+ * @property {NumKeyStrValuesAttributeType} disabled - Disabled text color attributes.
  */
 export interface TextColorInterface {
-    light: ColorAttributeType;
-    dark: ColorAttributeType;
-    disabled: ColorAttributeType;
+    light: NumKeyStrValuesAttributeType;
+    dark: NumKeyStrValuesAttributeType;
+    disabled: NumKeyStrValuesAttributeType;
 }
