fd4c5959de6da146c2c3b24740d84c226559a138
Daniel Andersson <d.andersson86@icloud.com>
2025-02-23 16:39:35 +0100

    update all docstrings in the module to be descriptive and working with JSDoc/TSDoc to generate developer docs, buttons.ts
    Took 4 minutes

--- assets/styles/buttons.ts
+++ assets/styles/buttons.ts
@@ -1,6 +1,14 @@
 /**
- * Module imports.
+ * @module buttons
+ *
+ * This module defines style attributes for various button components.
+ *
+ * It includes objects that specify the layout, appearance, and shadow properties for base
+ * buttons, list buttons, and button containers. These attributes are used to style the visual
+ * appearance and layout behavior of these button
+ * components in the user interface.
  */
+
 import * as Abstract from './abstracts';
 import * as Color from './colors';
 import * as Typography from './typography';
@@ -8,6 +16,25 @@ import * as Variable from './variables';
 
 /**
  * Base button properties.
+ *
+ * @property {string} overflow - The overflow behavior of the button.
+ * @property {string} width - The width of the button.
+ * @property {string} height - The height of the button.
+ * @property {number} paddingHorizontal - The horizontal padding inside the button.
+ * @property {number} paddingVertical - The vertical padding inside the button.
+ * @property {number} marginVertical - The vertical margin outside the button.
+ * @property {number} marginHorizontal - The horizontal margin outside the button.
+ * @property {string} alignSelf - The alignment of the button itself.
+ * @property {number} borderRadius - The border radius of the button.
+ * @property {string} color - The text color of the button.
+ * @property {string} backgroundColor - The background color of the button.
+ * @property {string} shadowColor - The color of the button shadow.
+ * @property {Object} shadowOffset - The offset of the button shadow.
+ * @property {number} shadowOffset.width - The horizontal offset of the button shadow.
+ * @property {number} shadowOffset.height - The vertical offset of the button shadow.
+ * @property {number} shadowOpacity - The opacity of the button shadow.
+ * @property {number} shadowRadius - The radius of the button shadow.
+ * @property {number} elevation - The elevation of the button.
  */
 export const baseButton = {
     overflow: 'hidden',
@@ -19,12 +46,8 @@ export const baseButton = {
     marginHorizontal: Typography.whiteSpace[10],
     alignSelf: 'center',
     borderRadius: Variable.borderRadius.button,
-
-    // Colors
     color: Color.text.dark,
     backgroundColor: Color.background.light,
-
-    // Shadows/Abstracts
     shadowColor: Color.shadows[400],
     shadowOffset: Abstract.shadow.btnOffset,
     shadowOpacity: Abstract.shadow.buttonOpacity,
@@ -32,19 +55,26 @@ export const baseButton = {
     elevation: Abstract.abstracts.buttonElevation,
 };
 
-// List button style
+/**
+ * List button properties.
+ *
+ * Inherits properties from `baseButton` and overrides the background color.
+ *
+ * @property {string} backgroundColor - The background color of the list button.
+ */
 export const listButton = {
     ...baseButton,
     backgroundColor: Color.schemeOne.primary[300],
 };
 
-// Button container style
+/**
+ * Button container properties.
+ *
+ * Inherits properties from `baseButton` and overrides the background color.
+ *
+ * @property {string} backgroundColor - The background color of the button container.
+ */
 export const buttonContainer = {
     ...baseButton,
     backgroundColor: Color.schemeOne.primary[300],
 };
-
-// export const warningButton = {
-//     ...baseButton,
-//     backgroundColor: Color.indicator.warning[300],
-// };
